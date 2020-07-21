08e1b3f84e2e598f930773ddd53a067b565970e8
Ben Chong <bensrww@gmail.com>
2020-07-22 00:23:27 +0800

    add ChessBoard page

---
+++ src/pages/chessBoard/ChessBoard.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import "./ChessBoard.css";
+
+export default class ChessBoard extends Component {
+  render() {
+    return <div>This is a chess board</div>;
+  }
+}
