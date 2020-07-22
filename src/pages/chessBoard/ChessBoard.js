bf50fc9a23ed113fc1d8b26967879222b93c87a9
Ben Chong <bensrww@gmail.com>
2020-07-22 22:43:30 +0800

    misc

--- src/pages/chessBoard/ChessBoard.js
+++ src/pages/chessBoard/ChessBoard.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
 import "./ChessBoard.css";
+import Xiangqi from "xiangqi.js";
+
+require("@lengyanyu258/xiangqiboardjs/src/xiangqiboard.js");
+const Xiangqiboard = window.Xiangqiboard;
 
 export default class ChessBoard extends Component {
   render() {
-    return <div>This is a chess board</div>;
+    const xiangqi = new Xiangqi();
+    const board1 = Xiangqiboard("board1", "start");
+    console.log("xiangqi", window, xiangqi);
+    return <div id="board1" style="width: 450px"></div>;
   }
 }
