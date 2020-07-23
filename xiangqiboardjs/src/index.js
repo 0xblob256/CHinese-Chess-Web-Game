adc582178bc13e597cf92b8747702bf64032f0e4
Ben Chong <bensrww@gmail.com>
2020-07-23 21:33:07 +0800

    init commit

--- xiangqiboardjs/src/index.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  boardTheme: "../docs/img/xiangqiboards/wikimedia/xiangqiboard.svg",
-  pieceTheme: "../docs/img/xiangqipieces/wikimedia/{piece}.svg",
-  orientation: "white",
-  position: "start",
-  showNotation: true,
-};
-
-Xiangqiboard("#myBoard1", config);
